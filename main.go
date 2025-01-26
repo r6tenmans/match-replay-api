ab22b5d6c3928025cadb222fe2ab1d747fff9535
calico-crusade <holycardboardquest@gmail.com>
2025-01-26 18:51:44 -0500

    Adding CORs preflights

--- main.go
+++ main.go
@@ -21,11 +21,28 @@ type RoundFile struct {
 func main() {
 	router := gin.Default()
 
+	router.Use(CORSMiddleware())
 	router.GET("/test", getTest)
 	router.POST("/round", postRound)
 	router.POST("/replay", postReplayZip)
 
 	router.Run(":8080")
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
 
 func getFile(c *gin.Context) (multipart.File, error) {
--- test.bat
+++ test.bat
@@ -1,3 +1,3 @@
 @echo off
 
-curl -X POST http://localhost:8080/replay -F "file=@F:\Games\Ubisoft\Tom Clancy's Rainbow Six Siege\MatchReplay\Match-2025-01-25_15-09-34-31184.zip" -H "Content-Type: multipart/form-data" -o "output.json"
+curl -X POST http://localhost:8080/replay -F "file=@F:\Games\Ubisoft\Tom Clancy's Rainbow Six Siege\MatchReplay\Match-2021-07-22_15-06-35-99.zip" -H "Content-Type: multipart/form-data" -o "output.json"
